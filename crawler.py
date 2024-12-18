c7fe35b8e1ded11180f4aa49475f1b1c92f0b3ea
koraygecimli <kgecimli@uni-osnabrueck.de>
2024-12-18 15:20:17 +0100

    creating extract and add function to store the data in the index, also created an index. implementation in while loop, good practice?

--- crawler.py
+++ crawler.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from index import extract, add_doc
 
 prefix = 'https://vm009.rz.uos.de/crawl/'
 
@@ -12,25 +13,30 @@ while agenda:
     url = agenda.pop()
     already_seen.append(url)
     print("Get ", url)
-    #request: library to perform web requests (to get html file)
+    # request: library to perform web requests (to get html file)
     r = requests.get(url)
     print(r, r.encoding)
     if r.status_code == 200 and r.headers.get('Content-Type', '').lower().startswith('text/html'):
-        #headers returns meta information (browser etc.)
+        # headers returns meta information (browser etc.)
         print(r.headers)
         soup = BeautifulSoup(r.content, 'html.parser')
-        #a: html element link
+
+        # Extract data and content
+        data = extract(url, soup)
+        add_doc(data)
+
+        # a: html element link
         all_links = soup.find_all('a')
         print(all_links)
         only_same_page_links = []
         for link in all_links:
             if not link['href'].startswith("http") or link['href'].startswith(prefix):
                 only_same_page_links.append(link)
-        print (soup.text)
+        print(soup.text)
         for link in only_same_page_links:
             if prefix + link['href'] not in already_seen and prefix + link['href'] not in agenda:
                 agenda.append(prefix + link['href'])
 
-
-        print (f"Agenda= {agenda}")
-        print (f"Already_seen={already_seen}")
+        print(f"Agenda= {agenda}")
+        print(f"Already_seen={already_seen}")
+        print(f"data={data}")
