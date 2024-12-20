1be8a96789807c9adcef9fc06d54283bfa98659b
koraygecimli <kgecimli@uni-osnabrueck.de>
2024-12-21 00:40:37 +0100

    template sachen angefnagen

--- webapp.py
+++ webapp.py
@@ -1,13 +1,9 @@
-from flask import Flask
+from flask import Flask, render_templates
 
 webapp = Flask(__name__)
 
 
 @webapp.route("/")
 def landing_page():
-    return """""
-        <form action="URL to send the information" method="GET or POST">
-            <input type="text" name="param">
-            <input type="submit">
-        </form>
-          """
+    return render_templates("lp.html")
+
