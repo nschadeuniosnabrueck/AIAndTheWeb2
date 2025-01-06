ad4c38618911268e2f1c8c09221f96cad189fde4
noahs <nschade@uni-osnabrueck.,de>
2025-01-06 14:02:12 +0100

    include stop words

--- index.py
+++ index.py
@@ -18,7 +18,7 @@ schema = Schema(
 # schema how the data will be stored in the index, will only be played one time now
 # TODO also check if index exists, not just folder
 if not os.path.exists("indexdir"):
-    schema = Schema(title=TEXT(stored=True), content=TEXT, url=TEXT(stored=True))
+    #schema = Schema(title=TEXT(stored=True), content=TEXT, url=TEXT(stored=True))
     os.mkdir("indexdir")
     ix = index.create_in("indexdir", schema) #creates the index
 
