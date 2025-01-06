e742257f516370ff806f5eab50ebcf66b2d8ae99
abrech <adrian.brechtken@gmail.com>
2025-01-06 13:48:41 +0100

    dont traverse links without href

--- crawler.py
+++ crawler.py
@@ -3,9 +3,9 @@ from bs4 import BeautifulSoup
 
 from index import add_doc
 
-prefix = 'https://vm009.rz.uos.de/crawl/'
+prefix = 'https://docs.python.org/3/'
 
-start_url = prefix + 'index.html'
+start_url = prefix + ''
 
 agenda = [start_url]
 already_seen = []
@@ -17,38 +17,40 @@ def extract(bs: BeautifulSoup, url: str):
     :param bs: BeautifulSoup object
     :return: dict containing title and content
     """
-    title = bs.title.string if bs.title else "No title"  # Falls kein Titel vorhanden
+    title = str(bs.title.string) if bs.title else "No title"  # Falls kein Titel vorhanden
     content = bs.get_text()
     return {"title": title, "content": content, "url": url}
+
+sites_crawled = 0
 
 while agenda:
     url = agenda.pop()
     already_seen.append(url)
-    print("Get ", url)
     # request: library to perform web requests (to get html file)
     r = requests.get(url)
-    print(r, r.encoding)
+    print(url)
     if r.status_code == 200 and r.headers.get('Content-Type', '').lower().startswith('text/html'):
         # headers returns meta information (browser etc.)
-        print(r.headers)
         soup = BeautifulSoup(r.content, 'html.parser')
 
         # Extract data and content
         data = extract(soup, url)
         add_doc(data)
-
+        sites_crawled += 1
         # a: html element link
         all_links = soup.find_all('a')
-        print(all_links)
         only_same_page_links = []
         for link in all_links:
+            if not link.has_attr('href'):
+                print("no href")
+                continue
             if not link['href'].startswith("http") or link['href'].startswith(prefix):
                 only_same_page_links.append(link)
-        print(soup.text)
         for link in only_same_page_links:
             if prefix + link['href'] not in already_seen and prefix + link['href'] not in agenda:
                 agenda.append(prefix + link['href'])
 
-        print(f"Agenda= {agenda}")
-        print(f"Already_seen={already_seen}")
-        print(f"data={data}")
+        #print(f"Agenda= {agenda}")
+        #print(f"Already_seen={already_seen}")
+        #print(f"data={data}")
+print(f'{sites_crawled} sites crawled')
