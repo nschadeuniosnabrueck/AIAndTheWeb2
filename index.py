bc1ecacb2f942bcda478b6ffb161de5bafaa693c
abrech <adrian.brechtken@gmail.com>
2025-01-08 16:35:53 +0100

    readme update

--- index.py
+++ index.py
@@ -1,18 +1,14 @@
-from whoosh.index import create_in
-from whoosh.fields import *
 from whoosh.qparser import QueryParser
 import os
 import whoosh.index as index
 
 from whoosh.fields import Schema, TEXT, ID
-from whoosh.analysis import RegexTokenizer, LowercaseFilter
 
 # TODO also check if index exists, not just folder
 if not os.path.exists("indexdir"):
     # Custom analyzer that doesn't remove stop words
-    custom_analyzer = RegexTokenizer() | LowercaseFilter()
-    schema = Schema(title=TEXT(stored=True, analyzer=custom_analyzer),
-                    content=TEXT(stored=True, analyzer=custom_analyzer),
+    schema = Schema(title=TEXT(stored=True),
+                    content=TEXT(stored=True),
                     url=ID(stored=True)) # we dont allow duplicates, so the url works as a ID
     os.mkdir("indexdir")
     ix = index.create_in("indexdir", schema)  #creates the index
